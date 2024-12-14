b9af5bff7579b89c93a304615ab779cbc9852767
Izazdev <izaz.khan@arhamsoft.com>
2024-12-14 15:29:15 +0400

    Text change

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -58,7 +58,7 @@ export default function AboutPage() {
                         to="/"
                         className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition"
                     >
-                        Explore the Menu
+                        Go to Home Page
                     </Link>
                 </div>
             </div>
